ab0ff767d81f0b2abdce85f01c57e0933f094d42
Tanuj <Tanuj@cmu.edu>
2024-04-09 16:26:20 -0400

    Stronger granular control from settings.json
    - Add to settings.json: k_excerpts, multivector framework, and hyperparameters
    - Stronger typechecking in classes.py

--- classes.py
+++ classes.py
@@ -3,7 +3,7 @@ from helpers import read_settings
 from typing import Literal
 from pydantic import BaseModel, Field
 
-LLM_FN = Literal[
+VALID_LLM = Literal[
     "get_openai_gpt4",
     "get_together_dolphin",
     "get_together_quen",
@@ -15,7 +15,7 @@ LLM_FN = Literal[
     "get_claude_sonnet",
     "get_local_model",
 ]
-EMBEDDER_FN = Literal["get_openai_embedder_large",
+VALID_EMBEDDER = Literal["get_openai_embedder_large",
                       "get_together_embedder_large",
                       "get_nomic_local_embedder"]
 
@@ -25,12 +25,15 @@ class RagSchema(BaseModel):
     Configuration for RAG
     """
     collection_name: str
-    embedding_model: EMBEDDER_FN
+    embedding_model: VALID_EMBEDDER
     method: Literal["faiss", "chroma"]
     chunk_size: int = Field(ge=0)
     chunk_overlap: int = Field(ge=0)
+    k_excerpts: int = Field(ge=0, le=8)
     rag_llm: str
     inputs: list[str]
+    multivector_enabled: bool
+    multivector_method: Literal["summary", "qa"]
     pass
 
 
@@ -38,14 +41,20 @@ class ChatSchema(BaseModel):
     """
     Configuration for Chat
     """
-    primary_model: LLM_FN
-    backup_model: LLM_FN
+    primary_model: VALID_LLM
+    backup_model: VALID_LLM
     enable_system_message: bool
     system_message: str
     rag_mode: bool
 
+class HyperparameterSchema(BaseModel):
+    """
+    Hyperparameters for LLM
+    """
+    max_tokens: int
+    temperature: float
 
-class Config():
+class Config:
     """
     Configuration class
     """
@@ -54,7 +63,21 @@ class Config():
         config = read_settings(config_file)
         self.rag_config = config["rag_config"]
         self.chat_config = config["chat_config"]
+        self.hyperparameters = config["hyperparameters"]
         self.__validate_configs()
+
+    def __check_context_max(self) -> bool:
+        """
+        Validate the context maxes
+        """
+        chunk_size = self.rag_config["chunk_size"]
+        k = self.rag_config["k_excerpts"]
+        # get context length of the model. For now, 128K to not throw errors
+        context_max = 128000
+        max_chars = context_max * 4
+        if chunk_size * k > max_chars:
+            return False
+        return True
 
     def __validate_configs(self):
         """
@@ -62,8 +85,11 @@ class Config():
         """
         RagSchema(**self.rag_config)
         ChatSchema(**self.chat_config)
+        HyperparameterSchema(**self.hyperparameters)
         # TODO:
         # Enforce chunk size check against the embedder here
+        if not self.__check_context_max():
+            raise ValueError("Context max exceeds model limit")
         pass
 
     def __str__(self):
@@ -75,6 +101,7 @@ class Config():
         """
         rag_config_str = "\n".join(f"{k}: {v}" for k, v in self.rag_config.items())
         chat_config_str = "\n".join(f"{k}: {v}" for k, v in self.chat_config.items())
+        hyperparameters_str = "\n".join(f"{k}: {v}" for k, v in self.hyperparameters.items())
         
-        return f"Rag Config:\n{rag_config_str}\n\nChat Config:\n{chat_config_str}"
+        return f"Rag Config:\n{rag_config_str}\n\nChat Config:\n{chat_config_str}\n\nHyperparameters:\n{hyperparameters_str}"
         
