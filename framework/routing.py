540389324b78cb0979f9175b27e430b299e7c280
Tanuj <Tanuj@cmu.edu>
2024-07-05 13:55:43 -0700

    more filepath shuffling

--- framework/routing.py
+++ framework/routing.py
@@ -2,15 +2,15 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 
-from models import get_together_bigmix, get_together_quen
+from models.models import get_together_bigmix, get_together_qwen
 
-SAMPLE_QUEN_QUERY = "Tell me a story about Quen the warrior!"
+SAMPLE_QWEN_QUERY = "Tell me a story about Qwen the warrior!"
 SAMPLE_LLAMA_QUERY = "What is a llama?"
 
 
 classifier_chain = (
     PromptTemplate.from_template(
-        """Given the user question below, classify it as either being about `Llama`, `Quen`, or `Other`.
+        """Given the user question below, classify it as either being about `Llama`, `Qwen`, or `Other`.
 
 Do not respond with more than one word.
 
@@ -20,7 +20,7 @@ Do not respond with more than one word.
 
 Classification:"""
     )
-    | get_together_quen()
+    | get_together_qwen()
     | StrOutputParser()
 )
 
@@ -31,15 +31,15 @@ Respond to the following question:
 
 Question: {question}
 Answer:"""
-) | get_together_quen()
-quen_chain = PromptTemplate.from_template(
-    """You are an expert in wizardly matters of Quen. \
+) | get_together_qwen()
+qwen_chain = PromptTemplate.from_template(
+    """You are an expert in wizardly matters of Qwen. \
 Always answer questions starting with "As Father Alibaba told me". \
 Respond to the following question:
 
 Question: {question}
 Answer:"""
-) | get_together_quen()
+) | get_together_qwen()
 general_chain = PromptTemplate.from_template(
     """Respond to the following question:
 
@@ -51,8 +51,8 @@ Answer:"""
 def route_topic_to_chain(info):
     if "llama" in info["topic"].lower():
         return llama_chain
-    elif "quen" in info["topic"].lower():
-        return quen_chain
+    elif "qwen" in info["topic"].lower():
+        return qwen_chain
     else:
         return general_chain
 
@@ -72,7 +72,7 @@ def test_first_chain(query = SAMPLE_LLAMA_QUERY):
     print("Passed!") if response_string else print("Failed!")
     return response_string
 
-def main(query = SAMPLE_QUEN_QUERY):
+def main(query = SAMPLE_QWEN_QUERY):
     if not query:
         print("No query provided")
         raise ValueError
